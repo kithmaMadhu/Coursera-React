b50cb5ef62fbaaa6ca40b8de3dc73b39395e4bfa
kithma <kithmamadushani1@gmail.com>
2020-10-16 00:16:22 +0530

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
 import './App.css';
 
 function App() {
@@ -10,7 +11,8 @@ function App() {
           <div className="container">
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
-        </Navbar>
+    </Navbar>
+    <Menu />
     </div>
   );
 }

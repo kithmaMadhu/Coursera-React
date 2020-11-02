32b7e23e461d150288749c577a56ae2b56960cc4
kithma <kithmamadushani1@gmail.com>
2020-11-02 23:13:42 +0530

    Fetch Post Comment

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,11 +10,11 @@ import Contact from './ContactComponent';
 import About from './AboutComponent';
 //import { addComment, fetchDishes } from '../redux/ActionCreators';
 import { actions } from 'react-redux-form';
-import { addComment, fetchDishes, fetchComments, fetchPromos } from '../redux/ActionCreators';
+import { postComment, fetchDishes, fetchComments, fetchPromos } from '../redux/ActionCreators';
 
 const mapDispatchToProps = dispatch => ({
   
-  addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
+  postComment: (dishId, rating, author, comment) => dispatch(postComment(dishId, rating, author, comment)),
   fetchDishes: () => { dispatch(fetchDishes())},
   resetFeedbackForm: () => { dispatch(actions.reset('feedback'))},
   fetchComments: () => dispatch(fetchComments()),
@@ -61,7 +61,7 @@ class Main extends Component {
           errMess={this.props.dishes.errMess}
           comments={this.props.comments.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
           commentsErrMess={this.props.comments.errMess}
-          addComment={this.props.addComment}
+          postComment={this.props.postComment}
         />
         );
       };
